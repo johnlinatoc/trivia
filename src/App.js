d5bf0e472be77533f8265cf43302373cb3825e30
John Linatoc <johnlinatoc1227@gmail.com>
2020-10-27 18:31:08 -0500

    Pass App.js data into child.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react';
+import QuestionsContainer from './questionsContainer/questionsContainer';
+
 const styles = require("./App.module.scss");
+const questions = require("./data/Apprentice_TandemFor400_Data.json");
 
 const App = () => {
+  const [data, setData] = useState(null)
+
+  const shuffleQuestions = questions => {
+    for (let i = questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+    return questions;
+}
+
+  useEffect(()=>{
+    const shuffledQuestions = shuffleQuestions(questions);
+    setData(shuffledQuestions.splice(0,10));
+  }, [])
+
   return (
     <div className={styles.app}>
-      Welcome
+      <h1>Welcome</h1>
+      {<QuestionsContainer data={data}/>}
     </div>
   );
 }
