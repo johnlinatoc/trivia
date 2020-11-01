250766a50f471b3375a6989161bcb30920febccd
John Linatoc <john.linatoc@hertz.com>
2020-11-01 07:06:12 -0600

    Complete initial styling of results page

--- src/resultsContainer/resultsContainer.js
+++ src/resultsContainer/resultsContainer.js
@@ -1,15 +1,17 @@
+const styles = require("./resultsContainer.module.scss");
+
 const ResultsContainer = (props) => {
-    const handleClick = () => {
-        props.restartGame();
-        props.showResults();
-    }
-    
-    return(<div>
-        <div>
-        You answered {props.results} out of 10 correct!
-        </div>
-        <button onClick={handleClick}>Return To Homescreen</button>
-    </div>)
-}
+  const handleClick = () => {
+    props.restartGame();
+    props.showResults();
+  };
+
+  return (
+    <div className={styles.results}>
+      <h1>You answered {props.results} out of 10 correct!</h1>
+      <button onClick={handleClick}>Return To Homescreen</button>
+    </div>
+  );
+};
 
 export default ResultsContainer;
